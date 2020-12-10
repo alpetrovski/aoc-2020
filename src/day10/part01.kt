13bdec7952d945adc61dbfe156a3e7a9b5018fdf
Aleksandar Petrovski <alpetrovski@users.noreply.github.com>
2020-12-10 21:30:45 +0100

    Day 10, part02 simplification.

--- src/day10/part01.kt
+++ src/day10/part01.kt
@@ -9,8 +9,8 @@ fun main() {
     }
 
     val result = orderedJoltages.zipWithNext { a, b -> b - a }.groupingBy { it }.eachCount()
-        .filter {entry -> entry.key == 1  || entry.key == 3}
-        .map { entry -> entry.value }
+        .filter { it.key == 1 || it.key == 3}
+        .map { it.value }
         .reduce { a, b -> a * b }
 
     println(result)
--- src/day10/part02.kt
+++ src/day10/part02.kt
@@ -4,24 +4,16 @@ import java.nio.file.Paths
 
 fun main() {
     val orderedJoltages = mutableListOf(0).apply { // start with 0
-        this.addAll(Paths.get("src/day10/input.in").toFile().readLines().map { it.toInt() })
+        this.addAll(Paths.get("src/day10/input.in").toFile().readLines().map { it.toInt() }.sorted())
         this.add(this.maxOrNull()!! + 3)
-    }.sorted()
+    }
 
     val state = mutableMapOf(orderedJoltages.size - 1 to 1L)
     for (i in orderedJoltages.size - 2 downTo 0) {
-        state[i] = calculateFromIndex(orderedJoltages, i, state)
+        state[i] = IntRange(i + 1, i + 3)
+            .filter { index -> index < orderedJoltages.size && orderedJoltages[index] - orderedJoltages[i] <= 3 }
+            .map { index -> state[index]!! }.sum()
     }
 
     println(state[0])
-}
-
-fun calculateFromIndex(orderedJoltages: List<Int>, fromIndex: Int, state: Map<Int, Long>): Long {
-    if (state.containsKey(fromIndex)) {
-        return state[fromIndex]!!
-    }
-
-    return IntRange(fromIndex + 1, fromIndex + 3)
-            .filter { index -> index < orderedJoltages.size && orderedJoltages[index] - orderedJoltages[fromIndex] <= 3 }
-            .map { index -> calculateFromIndex(orderedJoltages, index, state) }.sum()
 }
